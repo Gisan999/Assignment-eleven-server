eb294ed802c129904208bacbeeabe086049913a5
Gisan999 <ruheatheking3@gmail.com>
2023-11-08 19:49:03 +0600

    completly create json web token successfully

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// const secret = 'veryverysecretmaletakawkedewajabenadthadfhhedrfhdtyuh'
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 
-console.log();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csnq8lx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +30,29 @@ const client = new MongoClient(uri, {
     }
 });
 
+const logger = async (req, res, next) => {
+    console.log('called:', req.host, req.originalUrl);
+    next();
+}
+
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware', token);
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -32,26 +61,24 @@ async function run() {
         const wishListCollection = client.db('wishList').collection('wishes');
         const commentCollection = client.db('commentList').collection('comments');
 
-        const gateman = (req, res, next) => {
-            const { token } = req.cookies
+        // const gateman = (req, res, next) => {
+        //     const { token } = req.cookies
 
-            if (!token) {
-                return res.status(401).send({ message: 'you are not authorized' })
-            }
+        //     if (!token) {
+        //         return res.status(401).send({ message: 'you are not authorized' })
+        //     }
 
-            // verify a token symmetric
-            jwt.verify(token, secret, function (err, decoded) {
-                if (err) {
-                    return res.status(401).send({ message: 'you are not authorized' })
-                }
-                console.log(decoded) // bar
-                req.user = decoded
-                next();
-            });
+        //     // verify a token symmetric
+        //     jwt.verify(token, secret, function (err, decoded) {
+        //         if (err) {
+        //             return res.status(401).send({ message: 'you are not authorized' })
+        //         }
+        //         // console.log(decoded) // bar
+        //         req.user = decoded
+        //         next();
+        //     });
 
-        }
-
-
+        // }
 
 
 
@@ -61,7 +88,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/api/v1/blogs', async (req, res) => {
+        app.get('/api/v1/blogs', logger, async (req, res) => {
             const result = await blogsCollection.find().toArray();
             res.send(result);
         })
@@ -97,8 +124,18 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/api/v1/wishList', async (req, res) => {
-            const result = await wishListCollection.find().toArray();
+        app.get('/api/v1/wishList', logger, verifyToken, async (req, res) => {
+            // console.log('cook cook', req.cookies);
+            console.log('hello', req.user);
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const result = await wishListCollection.find(query).toArray();
             res.send(result)
         })
 
@@ -128,6 +165,37 @@ async function run() {
                 $expr: { $gt: [{ $strLenCP: "$longDescription" }, 10] }
             }).sort({ length: -1 }).limit(-10).toArray()
             res.send(result)
+        })
+
+        app.post('/api/v1/auth/jwt', async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: 60 * 60 })
+
+
+            // res.cookie(
+            //     "token",
+            //     tokenValue,
+            //     {
+            //     httpOnly: true,
+            //     secure: process.env.NODE_ENV,
+            //     sameSite: process.env.NODE_VNE,
+            //     }
+            //     )
+
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV,
+                sameSite: process.env.NODE_VNE,
+            }).send({ success: true })
+
+        })
+
+        app.post('/api/v1/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logout', user);
+            res.clearCookie('token', { maxAge: 0, secure: process.env.NODE_ENV, sameSite: process.env.NODE_VNE }).send({ success: true })
         })
 
         // app.get('/api/v1/feature', async (req, res) => {
@@ -165,8 +233,9 @@ async function run() {
         // await client.close();
     }
 }
-run().catch(console.dir);
 
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
